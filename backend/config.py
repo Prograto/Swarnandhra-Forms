036880e3505b045a83842616b477d5ece1df733d
Progrtao <chanduarasavalli95@gmail.com>
2026-01-07 14:47:52 +0530

    Use env vars for Mongo config; add backend .gitignore

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = "mongodb+srv://swarnandrianforms_db_user:Swarn2026@formsbyswarnandhra.jw3xrg9.mongodb.net/?appName=FormsBySwarnandhra"
-DB_NAME = "college_forms"
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME", "college_forms")
